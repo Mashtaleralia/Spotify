df1c844b2b37a65faea118e9f7f5338acb724aad
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2023-11-25 21:47:36 +0300

    onboarding vcs added

--- Spotify/Controllers/Other/SettingsViewController.swift
+++ Spotify/Controllers/Other/SettingsViewController.swift
@@ -7,23 +7,95 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    private var sections = [Section]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "Settings"
+        configureModels()
+        view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+        addConstraints()
+       
     }
     
+    private func configureModels() {
+        sections.append(Section(title: "Profile", options: [Option(title: "View your profile", handler: { [weak self] in
+            DispatchQueue.main.async {
+                self?.viewProfile()
+            }
+        })]))
+        
+        sections.append(Section(title: "Account", options: [Option(title: "Sign Out", handler: { [weak self] in
+            DispatchQueue.main.async {
+                self?.signOutTapped()
+            }
+        })]))
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+    
+    private func signOutTapped() {
+        
+    }
+    
+    private func viewProfile() {
+        let vc = ProfileViewController()
+        vc.title = "Profile"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+        ])
+    }
+  
+    // MARK: - TableView
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = sections[indexPath.section].options[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = model.title
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return sections[section].options.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let model = sections[indexPath.section].options[indexPath.row]
+        model.handler()
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let model = sections[section]
+        return model.title
+    }
+    
 
 }
--- Spotify/Controllers/Other/WelcomeViewController.swift
+++ Spotify/Controllers/Other/WelcomeViewController.swift
@@ -8,11 +8,58 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
+    
+    private let signInButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .white
+        button.setTitle("Sign In with Spotify", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Spotify"
         view.backgroundColor = .systemGreen
+        view.addSubview(signInButton)
+        addConstraints()
+        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            signInButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            signInButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            signInButton.widthAnchor.constraint(equalToConstant: view.frame.width - 40),
+            signInButton.heightAnchor.constraint(equalToConstant: 50),
+            
+        ])
+    }
+    
+    @objc private func didTapSignIn() {
+        let vc = AuthViewController()
+        vc.navigationItem.largeTitleDisplayMode = .never
+        vc.completionHandler = { [weak self] (success) in
+            DispatchQueue.main.async {
+                self?.handleSignIn(success: success)
+            }
+        }
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    private func handleSignIn(success: Bool) {
+        
+        guard success else {
+            let alert = UIAlertController(title: "oops!", message: "Something went wrong when signing in ", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+            present(alert, animated: true)
+            return
+        }
+        
+        let mainAppTabBarVC = TabBarController()
+        mainAppTabBarVC.modalPresentationStyle = .fullScreen
+        present(mainAppTabBarVC, animated: true)
     }
 
 }
