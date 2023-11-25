5bd67176a4ad11f0b699e96c52c0a3d8927e0407
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2023-11-25 21:41:08 +0300

    Profile vc added

--- Spotify/Controllers/Other/ProfileViewController.swift
+++ Spotify/Controllers/Other/ProfileViewController.swift
@@ -5,25 +5,101 @@
 //  Created by Admin on 16.10.2023.
 //
 
+import SDWebImage
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.isHidden = true
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
+    private var models = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "Profile"
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        fetchProfile()
+        view.backgroundColor = .systemBackground
+        addConstraints()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+        ])
     }
-    */
+    
+    private func fetchProfile() {
+        APICaller.shared.getCurrentUserProfile { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let model):
+                    self?.updateUI(with: model)
+                case .failure(let error):
+                    self?.failedToGetProfile()
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
 
+    private func updateUI(with model: UserProfile) {
+        tableView.isHidden = false
+        // configure table models
+        models.append("Full Name: \(model.display_name)")
+        models.append("User ID: \(model.id)")
+        models.append("Plan: \(model.product)")
+        createTableHeader(with: model.images.first?.url)
+        tableView.reloadData()
+    }
+    
+    private func createTableHeader(with string: String?) {
+        guard let urlString = string, let url = URL(string: urlString) else {
+            return
+        }
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.width / 1.5))
+        
+        let imageSize = headerView.frame.height / 2
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: imageSize, height: imageSize))
+        headerView.addSubview(imageView)
+        imageView.center = headerView.center
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = imageSize / 2.0
+        imageView.sd_setImage(with: url, completed: nil)
+        tableView.tableHeaderView = headerView
+    }
+    
+    private func failedToGetProfile() {
+        let label = UILabel()
+        label.text = "Failed to load profile."
+        label.sizeToFit()
+        label.textColor = .secondaryLabel
+        view.addSubview(label)
+        label.center = view.center
+    }
+    
+    //MARK: - TableView
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = models[indexPath.row]
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
 }
