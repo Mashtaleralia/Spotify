557433f13123a648c6ce312b51d39394f5d9988f
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2024-02-04 16:47:12 +0300

    data formatting extensions added

---
+++ Spotify/Resources/Extensions.swift
@@ -0,0 +1,31 @@
+//
+//  Extensions.swift
+//  Spotify
+//
+//  Created by Admin on 11.11.2023.
+//
+
+import Foundation
+
+extension DateFormatter {
+    static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "YYYY-MM-dd"
+        return formatter
+    }()
+    
+    static let displayDateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        return formatter
+    }()
+}
+
+extension String {
+    static func formattedDate(string: String) -> String {
+        guard let date = DateFormatter.dateFormatter.date(from: string) else {
+            return string
+        }
+        return DateFormatter.displayDateFormatter.string(from: date)
+    }
+}
