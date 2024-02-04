680d9e7b8da2b72d796a02098d6a5ac19b7c94d7
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2024-02-04 16:51:47 +0300

    view model for player screen

---
+++ Spotify/ViewModels/PlaylistHeaderViewViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  PlaylistHeaderViewViewModel.swift
+//  Spotify
+//
+//  Created by Admin on 06.11.2023.
+//
+
+import Foundation
+
+struct PlaylistHeaderViewViewModel {
+    let name: String?
+    let ownerName: String?
+    let description: String?
+    let artworkURL: URL?
+}
