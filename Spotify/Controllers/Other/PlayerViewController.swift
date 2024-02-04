747457adc6152fc566528d5dbb0751a3a3245ee1
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2024-02-04 16:51:08 +0300

    player controller for playback

--- Spotify/Controllers/Other/PlayerViewController.swift
+++ Spotify/Controllers/Other/PlayerViewController.swift
@@ -6,24 +6,98 @@
 //
 
 import UIKit
+import SDWebImage
 
+protocol PlayerViewControllerDelegate: AnyObject {
+    func didTapPlayPause()
+    func didTapForward()
+    func didTapBackward()
+    func didSlideSlider(_ value: Float)
+}
+ 
 class PlayerViewController: UIViewController {
+    
+    weak var dataSource: PlayerDataSource?
+    
+    weak var delegate: PlayerViewControllerDelegate?
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private var controlsView = PlayerControlsView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        view.addSubview(imageView)
+        view.addSubview(controlsView)
+        controlsView.delegate = self
+        addConstraints()
+        configureBarButtons()
+        configure()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    public func refreshUI() {
+        
+        configure()
     }
-    */
+    
+    private func addConstraints() {
+        let controlsHeight = view.frame.height - imageView.frame.height - 15 - view.safeAreaInsets.bottom - view.safeAreaInsets.top
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: view.safeAreaInsets.top),
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: view.frame.width),
+            imageView.heightAnchor.constraint(equalToConstant: view.frame.width),
+            controlsView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            controlsView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 10),
+            controlsView.widthAnchor.constraint(equalToConstant: view.frame.width - 20),
+            controlsView.heightAnchor.constraint(equalToConstant: controlsHeight)
+        ])
+    }
+    
+    private func configure() {
+        imageView.sd_setImage(with: dataSource?.imageURL, completed: nil)
+        controlsView.configure(with: PlayerControlsViewViewModel(
+            title: dataSource?.song,
+            subtitle: dataSource?.subtitle))
+    }
+ 
+    private func configureBarButtons() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(didTapClose))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(didTapAdd))
+    }
+    
+    @objc private func didTapAdd() {
+        
+    }
+    
+    @objc private func didTapClose() {
+        dismiss(animated: true, completion: nil)
+    }
+}
 
+extension PlayerViewController: PlayerControlsViewDelegate {
+    
+    func playerControlsViewDidSlideVolume(_ playerControlsView: PlayerControlsView, didSlideSlider volume: Float) {
+        delegate?.didSlideSlider(volume)
+    }
+    
+    func playerControlsViewDidTapPlayPauseButton(_ playerControlsView: PlayerControlsView) {
+        delegate?.didTapPlayPause()
+    }
+    
+    func playerControlsViewDidTapForwardButton(_ playerControlsView: PlayerControlsView) {
+        delegate?.didTapForward()
+    }
+    
+    func playerControlsViewDidTapBackwardButton(_ playerControlsView: PlayerControlsView) {
+        delegate?.didTapBackward()
+    }
+    
+    
 }
