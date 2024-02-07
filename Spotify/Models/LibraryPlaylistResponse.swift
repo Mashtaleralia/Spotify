818b48e40cbacc7b82d0d4a45e5e4b0e4b3cf434
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2024-02-07 12:26:49 +0300

    data model for playlist response added

---
+++ Spotify/Models/LibraryPlaylistResponse.swift
@@ -0,0 +1,13 @@
+//
+//  LibraryPlaylistResponse.swift
+//  Spotify
+//
+//  Created by Admin on 04.02.2024.
+//
+
+import Foundation
+
+struct LibraryPlaylistResponse: Codable {
+    let items: [Playlist]
+    
+}
